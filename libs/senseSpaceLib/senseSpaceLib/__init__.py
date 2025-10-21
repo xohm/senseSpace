60e0a6cdb85b4c5b39b6aed26b55210f90f597d4
Max Rheiner <xohm@xocog.org>
2025-10-21 11:36:20 +0200

    extended lib paths

---
+++ libs/senseSpaceLib/senseSpaceLib/__init__.py
@@ -0,0 +1,15 @@
+"""
+senseSpaceLib - Shared protocol and data structures for senseSpace
+"""
+
+__version__ = "0.1.0"
+
+# Import main components (add your actual modules here)
+# Example:
+# from .protocol import Protocol
+# from .data_structures import DataStructure
+
+__all__ = [
+    '__version__',
+    # Add your exported classes/functions here
+]
--- libs/senseSpaceLib/setup.py
+++ libs/senseSpaceLib/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="senseSpaceLib",
@@ -6,7 +6,10 @@ setup(
     description="Shared protocol and data structures for senseSpace server and client.",
     author="Max Rheiner",
     author_email="max.rheiner@zhdk.ch",
-    packages=find_packages(),
+    # Explicitly specify the package
+    packages=["senseSpaceLib"],
+    # Tell setuptools where to find it
+    package_dir={"senseSpaceLib": "senseSpaceLib"},
     include_package_data=True,
     install_requires=[],
     python_requires=">=3.7",
